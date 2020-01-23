56714fa6a2f2800ab2cac620ed98c863c41a001a
Milan <milan@milannombp.myfiosgateway.com>
2020-01-23 16:01:32 -0500

    Updates

---
+++ precache-manifest.14255c26fd28ef2a9a9f3bf2f530b533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dc9a39b7b27efa6f450dd3d61b9113",
+    "url": "/Meat-Consumption-Data-Visualization/index.html"
+  },
+  {
+    "revision": "5dda21917735181fd11d",
+    "url": "/Meat-Consumption-Data-Visualization/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "e261f2fc5c51e84fc434",
+    "url": "/Meat-Consumption-Data-Visualization/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5dda21917735181fd11d",
+    "url": "/Meat-Consumption-Data-Visualization/static/js/2.db86dc83.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Meat-Consumption-Data-Visualization/static/js/2.db86dc83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e261f2fc5c51e84fc434",
+    "url": "/Meat-Consumption-Data-Visualization/static/js/main.fd7a88e1.chunk.js"
+  },
+  {
+    "revision": "c17074724c7fb5f141b5",
+    "url": "/Meat-Consumption-Data-Visualization/static/js/runtime-main.897c5ccd.js"
+  }
+]);
